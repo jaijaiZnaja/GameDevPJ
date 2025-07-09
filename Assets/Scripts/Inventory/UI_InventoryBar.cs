de3db7e7811638eaac223d6f5804c30812b76d00
jaijaiZnaja <128208041+jaijaiZnaja@users.noreply.github.com>
2025-07-09 19:51:54 +0700

    [Feature] Add inventory bar
    10 slots inventory bar
    20 bag slot
    slow icon and item count
    add font Roboto to unity

--- Assets/Scripts/Inventory/UI_InventoryBar.cs
+++ Assets/Scripts/Inventory/UI_InventoryBar.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class UI_InventoryBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("UI Settings")]
+    [SerializeField] private GameObject slotPrefab;
+    [SerializeField] private Transform slotContainer;
+    [Tooltip("จำนวนช่องที่จะแสดงผลในแถบ Inventory Bar")]
+    [SerializeField] private int slotsToDisplay = 10;
+
+    private List<UI_InventorySlot> uiSlots = new List<UI_InventorySlot>();
+
+    private void Start()
     {
-        
+        InventoryManager.Instance.OnInventoryChanged.AddListener(Redraw);
+        CreateSlots();
+        Redraw();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        if (InventoryManager.Instance != null)
+        {
+            InventoryManager.Instance.OnInventoryChanged.RemoveListener(Redraw);
+        }
+    }
+    private void CreateSlots()
+    {
+        foreach (Transform child in slotContainer)
+        {
+            Destroy(child.gameObject);
+        }
+        uiSlots.Clear();
+
+        for (int i = 0; i < slotsToDisplay; i++)
+        {
+            if (i < InventoryManager.Instance.inventorySlots.Count)
+            {
+                GameObject newSlot = Instantiate(slotPrefab, slotContainer);
+                uiSlots.Add(newSlot.GetComponent<UI_InventorySlot>());
+            }
+        }
+    }
+    public void Redraw()
+    {
+        for (int i = 0; i < uiSlots.Count; i++)
+        {
+            uiSlots[i].UpdateSlot(InventoryManager.Instance.inventorySlots[i]);
+        }
     }
 }
--- Assets/Scripts/Inventory/UI_InventorySlot.cs
+++ Assets/Scripts/Inventory/UI_InventorySlot.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UI_InventorySlot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] public Image icon;
+    [SerializeField] public TextMeshProUGUI quantityText;
+
+    public void UpdateSlot(InventorySlot slot)
     {
-        
+        if (slot.itemData != null && slot.quantity > 0)
+        {
+            // Update icon
+            icon.sprite = slot.itemData.icon;
+            icon.enabled = true;
+
+            if (slot.quantity > 1)
+            {
+                quantityText.text = slot.quantity.ToString();
+                quantityText.enabled = true;
+            }
+            else
+            {
+                quantityText.enabled = false;
+            }
+        }
+        else
+        {
+            // empty slot
+            icon.sprite = null;
+            icon.enabled = false;
+            quantityText.enabled = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
